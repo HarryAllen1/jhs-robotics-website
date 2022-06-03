8e01970b6879aa0fe447a1c9f566bfc2692795fe
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-06-02 21:03:20 -0700

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<marquee style="font-size: xx-large; font-weight: bold;">JHS Robotics Club</marquee>
+<a href="https://gofund.me/4048a9e8">go donate now</a>
+
+<style global>
+	html {
+		background-color: grey;
+	}
+</style>
