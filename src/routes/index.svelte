a83c976d43e8153af44e935a55c475fca6a4fb7a
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-06-02 21:17:21 -0700

    iframe

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
 <marquee style="font-size: xx-large; font-weight: bold;">JHS Robotics Club</marquee>
 <a href="https://gofund.me/4048a9e8">go donate now</a>
+<iframe
+	src="https://www.gofundme.com/f/jhs-robotics-club/widget/large/"
+	title="Go Fund Me"
+	frameborder="0"
+	height="500"
+/>
