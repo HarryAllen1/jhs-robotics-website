3484e95d045c914abe8d96618ce5271b5705efc0
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-06-05 19:02:25 -0700

    ???

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,6 @@
-<svelte:head>Home - JHS Robotics Club</svelte:head>
+<svelte:head>
+  <title>Home - JHS Robotics Club</title>
+</svelte:head>
 
 <h1>First meeting:</h1>
 <p>June 7th in room 166. 2:50-3:30 pm</p>
