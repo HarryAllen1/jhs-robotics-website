062677856a31c757a554259994d3d0e6a057cdd7
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-06-09 15:25:28 -0700

    Update logo and donate instructions

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,10 +4,10 @@
   import '../app.scss';
 </script>
 
-<nav class="top-0 mb-4 px-6 text-xl text-black w-full border-b z-20">
+<nav class="top-0 mb-4 px-6 text-2xl text-black w-full border-b z-20">
   <div class="py-4 pr-4 flex justify-between items-center">
     <div class="flex items-center space-x-4 md:space-x-0">
-      <a href="/" class="flex flex-row items-center">
+      <a href="/" class="flex flex-row text-2xl items-center">
         <img src={logo} alt="JHS Robotics Logo" class="h-16 mr-4" />
         <span class="text-red-600 font-bold">JHS</span>
         &nbsp;
@@ -18,7 +18,7 @@
     <div class="flex-grow" />
 
     <a class="mx-2" href="https://discord.gg/FdFkZFJa3H" target="_blank">Discord Server</a>
-    <a class="mx-2" href="https://gofund.me/4048a9e8">Go Fund Me</a>
+    <a class="mx-2" href="/donate">Donate</a>
   </div>
 </nav>
 
---
+++ src/routes/donate.svelte
@@ -0,0 +1,8 @@
+<h1>Donate to the Club!</h1>
+<p>
+  Currently, the GoFundMe has been shut down since it is near impossible for the bookkeeper to keep
+  track of money.
+  <br />
+  <br />
+  Instead, please give money to the bookkeeper asking to donate to the JHS Robotics Club.
+</p>
