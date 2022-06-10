8f28b54d27e842a6a281af3cd914cf4a1d134283
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-06-09 17:15:08 -0700

    responsive

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,11 +4,11 @@
   import '../app.scss';
 </script>
 
-<nav class="top-0 mb-4 px-6 text-2xl text-black w-full border-b z-20">
+<nav class="top-0 mb-4 px-6 text-lg sm:text-xl md:text-2xl text-black w-full border-b z-20">
   <div class="py-4 pr-4 flex justify-between items-center">
     <div class="flex items-center space-x-4 md:space-x-0">
-      <a href="/" class="flex flex-row text-2xl items-center">
-        <img src={logo} alt="JHS Robotics Logo" class="h-16 mr-4" />
+      <a href="/" class="flex flex-row items-center">
+        <img src={logo} alt="JHS Robotics Logo" class="h-16 mr-4 hidden md:block" />
         <span class="text-red-600 font-bold">JHS</span>
         &nbsp;
         <span class="text-blue-800">Robotics Club</span>
@@ -17,8 +17,8 @@
 
     <div class="flex-grow" />
 
-    <a class="mx-2" href="https://discord.gg/FdFkZFJa3H" target="_blank">Discord Server</a>
-    <a class="mx-2" href="/donate">Donate</a>
+    <a class="mx-1 md:mx-2" href="https://discord.gg/FdFkZFJa3H" target="_blank">Discord Server</a>
+    <a class="mx-1 md:mx-2" href="/donate">Donate</a>
   </div>
 </nav>
 
