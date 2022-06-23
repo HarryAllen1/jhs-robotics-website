176750a42c3e53a39c5539be13fbc6bf6f105b07
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-06-22 20:51:55 -0700

    remove  brs lol

--- src/routes/donate.svelte
+++ src/routes/donate.svelte
@@ -9,7 +9,5 @@
 <p>
   Currently, the GoFundMe has been shut down since it is near impossible for the bookkeeper to keep
   track of money.
-  <br />
-  <br />
-  Instead, please give money to the bookkeeper asking to donate to the JHS Robotics Club.
 </p>
+<p>Instead, please give money to the bookkeeper asking to donate to the JHS Robotics Club.</p>
