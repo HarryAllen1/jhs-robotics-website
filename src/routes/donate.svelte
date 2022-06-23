9da4732b5ddaa90afbcdb3d9e6744d3898a2d32f
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-06-22 21:12:29 -0700

    hehehaw

--- src/routes/donate.svelte
+++ src/routes/donate.svelte
@@ -6,8 +6,4 @@
 </svelte:head>
 
 <h1>Donate to the Club!</h1>
-<p>
-  Currently, the GoFundMe has been shut down since it is near impossible for the bookkeeper to keep
-  track of money.
-</p>
-<p>Instead, please give money to the bookkeeper asking to donate to the JHS Robotics Club.</p>
+<p>you currently can't donate lmao</p>
