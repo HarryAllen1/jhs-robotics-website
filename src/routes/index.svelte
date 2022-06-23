2673c3466b2f09fe18d5e8d8e0747103f928e253
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-06-23 13:06:43 -0700

    _blank

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,8 +9,9 @@
 <p>June 7th in room 166. 2:50-3:30 pm</p>
 <h2>Information</h2>
 <p>
-  For this club, we will be using <a href="https://www.vexrobotics.com/v5">Vex V5</a>. The club will
-  likely be split into 2 groups:
+  For this club, we will be using <a target="_blank" href="https://www.vexrobotics.com/v5">
+    Vex V5
+  </a>. The club will likely be split into 2 groups:
 </p>
 <ul>
   <li>A casual group that will be learning about the robot</li>
